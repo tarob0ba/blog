4cced98430bacc79f4208f5f1ad814c91b21a386
Beckett Normington <beckett@chatter-social.com>
2022-04-12 09:36:47 -0400

    typo fix

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,7 +1,7 @@
 const projectsData = [
   {
     title: 'The Pearl',
-    description: `The blog that you are reading, written with love with in Next.js and TailwindCSS.`,
+    description: `The blog that you are reading, written with love with Next.js and TailwindCSS.`,
     imgSrc: '/static/images/pearl.png',
     href: 'https://b0ba.dev',
   },
