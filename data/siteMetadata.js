029a3b76b350ed3429a45c818145927b495a1b6d
Beckett Normington <beckett@chatter-social.com>
2022-06-18 20:27:16 -0400

    use umami analytics

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -20,7 +20,7 @@ const siteMetadata = {
     // supports plausible, simpleAnalytics, umami or googleAnalytics
     plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
     simpleAnalytics: false, // true or false
-    umamiWebsiteId: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
+    umamiWebsiteId: 'b49c2638-7c90-4db0-9220-59a8ec17bd94', // e.g. 123e4567-e89b-12d3-a456-426614174000
     googleAnalyticsId: '', // e.g. UA-000000-2 or G-XXXXXXX
   },
   newsletter: {
